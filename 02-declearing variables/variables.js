38979ba3f2662466805056937ea9c55d42c82508
Anshul <anshul524@gmail.com>
2020-06-26 12:14:21 +0530

    declearing variables

---
+++ 02-declearing variables/variables.js
@@ -0,0 +1,7 @@
+function doSomething() {
+    for (var index = 0; index < 5; index++) {
+        console.log(index);
+    }
+    console.log("Finally " + index);
+}
+doSomething();
---
+++ 02-declearing variables/variables.ts
@@ -0,0 +1,9 @@
+function doSomething(){
+    for (var index = 0; index < 5; index++) {
+        console.log(index);
+        
+    }
+    console.log("Finally " + index);
+}
+
+doSomething();
