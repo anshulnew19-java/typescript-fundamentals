a06d0920389f283ea702248703034b8e90d489fc
Anshul <anshul524@gmail.com>
2020-07-02 11:45:07 +0530

    TS Optional Constructor

---
+++ 08-constructor/constructor.ts
@@ -0,0 +1,16 @@
+class Point{
+    x: number;
+    y: number;
+
+    constructor(x:number, y:number){
+        this.x = x;
+        this.y = y;
+    }
+
+    draw() {
+        console.log("X is : " + this.x + " "+ "Y is : " + this.y);
+    }
+}
+
+let point = new Point(10,20);
+point.draw();
